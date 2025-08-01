70b30cf72aa6ac76943ee3e3fd15a9f9faee96a4
Jamil <Jamilw284@gmail.com>
2025-07-31 20:34:27 -0500

    Fully Created the BankContext file - last prerequesite before SQLite db setup

--- backend/data/BankContext.cs
+++ backend/data/BankContext.cs
@@ -2,5 +2,9 @@ using Microsoft.EntityFrameworkCore;
 using Backend.data.entities;
 
 public class BankContext : DbContext{
-    
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<AccountEntity> Accounts { get; set; }
+    public DbSet<TransactionEntity> Transactions { get; set; }
+
+    public BankContext(DbContextOptions<BankContext> options) : base(options){}
 }
