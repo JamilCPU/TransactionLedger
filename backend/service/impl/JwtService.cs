320ad3e4de47f7a2201a160763f65f4f1d0a5f24
Jamil <Jamilw284@gmail.com>
2025-08-10 23:16:18 -0500

    Created JwtService class and interface to handle token creation when user logs in

---
+++ backend/service/impl/JwtService.cs
@@ -0,0 +1,11 @@
+using Backend.service.intrface;
+namespace Backend.service.impl
+{
+    public class JwtService : IJwtService
+    {
+        public string GenerateToken()
+        {
+            return "";
+        }
+    }
+}
---
+++ backend/service/interface/IJwtService.cs
@@ -0,0 +1,3 @@
+public interface IJwtService{
+    public string GenerateToken();
+}
