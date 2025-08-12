a096686f3ab5fd7b46756c1ffc2b8ea9f67fc3d0
Jamil <Jamilw284@gmail.com>
2025-08-11 23:08:28 -0500

    Created JwtOptions class that will hold JWT info

--- backend/service/impl/JwtService.cs
+++ backend/service/impl/JwtService.cs
@@ -1,8 +1,11 @@
 using Backend.service.intrface;
+
 namespace Backend.service.impl
 {
     public class JwtService : IJwtService
     {
+            private readonly JwtOptions _opt;
+
         public string GenerateToken()
         {
             return "";
