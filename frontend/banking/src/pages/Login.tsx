b32d226d7e105863a6e436555999621f52ea695f
Jamil <Jamilw284@gmail.com>
2025-08-25 16:09:35 -0500

    Refined Element Placement, Sizes, and Spacing in the LoginPage

--- frontend/banking/src/pages/Login.tsx
+++ frontend/banking/src/pages/Login.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 const Login: React.FC = () => {
     return (
-        <div className = "flex flex-col items-center justify-center min-h-[70vh] gap-4">
-            <h1 className = "text-2xl font-bold">Username</h1>
-            <input type = "text" className = "border-2 border-gray-300 rounded-md p-2" />
-            <h1 className = "text-2xl font-bold">Password</h1>
-            <input type = "password" className = "border-2 border-gray-300 rounded-md p-2" />
-            <button className = "bg-blue-500 text-white p-2 rounded-md">Login</button>
+        <div className="flex flex-col justify-center min-h-[70vh] gap-4">
+            <form className="flex flex-col justify-center gap-4">
+                <div className="mx-auto">
+                    <h1 className="text-2xl font-bold mb-2">Username</h1>
+                    <input type="text" className="border-2 border-gray-300 rounded-md p-2" />
+                </div>
+                <div className="mx-auto">
+                    <h1 className="text-2xl font-bold mb-2">Password</h1>
+                    <input type="password" className="border-2 border-gray-300 rounded-md p-2" />
+                </div>
+                <button className="bg-blue-500 text-white p-2 rounded-md w-28 mx-auto mt-6">Login</button>
+            </form>
         </div>
     );
 };
