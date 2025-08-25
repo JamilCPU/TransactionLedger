2c8119798a532cff2cf1efa08d02d374c9c6e5e5
Jamil <Jamilw284@gmail.com>
2025-08-25 15:35:59 -0500

    Theming Update
    Created Base Login Page Elements

--- frontend/banking/src/pages/Login.tsx
+++ frontend/banking/src/pages/Login.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Login: React.FC = () => {
     return (
-        <div>
-            <h1>Base Login Page</h1>
+        <div className = "flex flex-col items-center justify-center min-h-[70vh] gap-4">
+            <h1 className = "text-2xl font-bold">Username</h1>
+            <input type = "text" className = "border-2 border-gray-300 rounded-md p-2" />
+            <h1 className = "text-2xl font-bold">Password</h1>
+            <input type = "password" className = "border-2 border-gray-300 rounded-md p-2" />
+            <button className = "bg-blue-500 text-white p-2 rounded-md">Login</button>
         </div>
     );
 };
