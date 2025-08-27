b724b74589388e2462ae98ce8a4a205339ea4874
Jamil <Jamilw284@gmail.com>
2025-08-26 22:22:12 -0500

    Added representations of input fields as React states

--- frontend/banking/src/pages/Login.tsx
+++ frontend/banking/src/pages/Login.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login: React.FC = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
     const tryLogin = () => {
         
     }
@@ -11,11 +15,11 @@ const Login: React.FC = () => {
             <form className="flex flex-col justify-center gap-4">
                 <div className="mx-auto">
                     <h1 className="text-2xl font-bold mb-2">Username</h1>
-                    <input type="text" className="border-2 border-gray-300 rounded-md p-2" />
+                    <input type="text" className="border-2 border-gray-300 rounded-md p-2" onChange={(input) => setUsername(input.target.value)}/>
                 </div>
                 <div className="mx-auto">
                     <h1 className="text-2xl font-bold mb-2">Password</h1>
-                    <input type="password" className="border-2 border-gray-300 rounded-md p-2" />
+                    <input type="password" className="border-2 border-gray-300 rounded-md p-2" onChange={(input) => setPassword(input.target.value)}/>
                 </div>
                 <button className="bg-blue-500 text-white p-2 rounded-md w-28 mx-auto mt-6">Login</button>
             </form>
