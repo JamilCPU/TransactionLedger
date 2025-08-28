7951ad88439839177b03e139f5cf73244442c3b2
Jamil <Jamilw284@gmail.com>
2025-08-27 21:50:59 -0500

    Implemented the tryLogin function into the form

--- frontend/banking/src/pages/Login.tsx
+++ frontend/banking/src/pages/Login.tsx
@@ -6,13 +6,18 @@ const Login: React.FC = () => {
 
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState('');
+    
     const tryLogin = () => {
-        
+        console.log(username, password);
+        setIsLoading(true);
     }
 
     return (
         <div className="flex flex-col justify-center min-h-[70vh] gap-4">
-            <form className="flex flex-col justify-center gap-4">
+            <form className="flex flex-col justify-center gap-4" onSubmit={(form) => {
+                form.preventDefault();
+                tryLogin();
+            }}>
                 <div className="mx-auto">
                     <h1 className="text-2xl font-bold mb-2">Username</h1>
                     <input type="text" className="border-2 border-gray-300 rounded-md p-2" onChange={(input) => setUsername(input.target.value)}/>
@@ -21,7 +26,7 @@ const Login: React.FC = () => {
                     <h1 className="text-2xl font-bold mb-2">Password</h1>
                     <input type="password" className="border-2 border-gray-300 rounded-md p-2" onChange={(input) => setPassword(input.target.value)}/>
                 </div>
-                <button className="bg-blue-500 text-white p-2 rounded-md w-28 mx-auto mt-6">Login</button>
+                <button className="bg-blue-500 text-white p-2 rounded-md w-28 mx-auto mt-6" type="submit" disabled={isLoading}>Login</button>
             </form>
         </div>
     );
